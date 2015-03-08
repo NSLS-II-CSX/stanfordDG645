47575040a93f424ab43a298679de9ff5f4fc52b8
Stuart B. Wilkins <swilkins@bnl.gov>
2015-03-08 16:15:06 -0400

    ENH : First checkin of working version

--- iocBoot/iocstanfordDG645/st.cmd
+++ iocBoot/iocstanfordDG645/st.cmd
@@ -3,16 +3,49 @@
 ## You may have to change stanfordDG645 to something else
 ## everywhere it appears in this file
 
-#< envPaths
+< envPaths
+
+epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST" , "NO")
+epicsEnvSet("EPICS_CA_ADDR_LIST"      , "10.23.0.255")
 
 ## Register all support components
 dbLoadDatabase("../../dbd/stanfordDG645.dbd",0,0)
 stanfordDG645_registerRecordDeviceDriver(pdbbase) 
 
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/proto")
+drvAsynIPPortConfigure("IP1", "10.23.3.12:5025")
+#asynSetTraceMask("IP1", 0, 0x9)
+#asynSetTraceIOMask("IP1", 0, 0x2)
+drvAsynIPPortConfigure("IP2", "10.23.3.13:5025")
+#asynSetTraceMask("IP2", 0, 0x9)
+#asynSetTraceIOMask("IP2", 0, 0x2)
+
+# Change into TOP
+cd("$(TOP)")
+
 ## Load record instances
-dbLoadRecords("../../db/stanfordDG645.db","user=swilkins")
+dbLoadTemplate("db/dg645_delay.substitutions")
+
+asSetFilename("/epics/xf/23id/xf23id.acf")
+
+system("install -m 777 -d $(TOP)/as/save") 
+system("install -m 777 -d $(TOP)/as/req")
+
+set_savefile_path("${TOP}/as","/save")
+set_requestfile_path("${TOP}/as","/req")
+set_pass0_restoreFile("info_positions.sav")
+set_pass1_restoreFile("info_settings.sav")
 
 iocInit()
 
-## Start any sequence programs
-#seq sncstanfordDG645,"user=swilkins"
+caPutLogInit("xf23id-ca.cs.nsls2.local:7004", 0)
+
+cd ${TOP}/as/req
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5 , "")
+create_monitor_set("info_settings.req", 15 , "")
+
+dbl > ${TOP}/records.dbl
+system("cp ${TOP}/records.dbl /cf-update/xf23id1-ioc2.es-dg645.dbl")
+
+
---
+++ st.cmd
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd iocBoot/iocstanfordDG645 && ./st.cmd
+
